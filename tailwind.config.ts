496e225cebe1115c28c9fbaaa7f4e6f36f81a897
tejas-git64 <tejas0586@gmail.com>
2025-02-18 20:22:10 +0530

    updated tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
