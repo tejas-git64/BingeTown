73e590c091a3d819e38a9dd03ee74806f00948e9
tejas-git64 <tejas0586@gmail.com>
2026-04-22 00:13:36 +0530

    added signal

--- api/requests.ts
+++ api/requests.ts
@@ -1,9 +1,10 @@
 "use server";
 import { options } from "./headers";
 
-export async function getMediaData(url: string) {
+export async function getMediaData(url: string, signal?: AbortSignal) {
+  let updatedOptions = signal ? { ...options, signal } : options;
   try {
-    const res = await fetch(url, options);
+    const res = await fetch(url, updatedOptions);
     const data = await res.json();
     return data;
   } catch (err: any) {
